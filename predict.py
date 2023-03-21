01f57bded9607ba57aa966e664cbfa5df6671b8f
arsh73552 <arsh01042002@gmail.com>
2023-03-21 12:14:56 +0530

    Predictions on  50 example Images included, JIT attempt

--- predict.py
+++ predict.py
@@ -11,35 +11,40 @@ real_dim = 3
 lr = 0.0003
 target_shape = 256
 device = 'cpu'
-
-img = Image.open("2.jpg")
-convert_tensor = transforms.ToTensor()
-image = convert_tensor(img)
-image = torch.unsqueeze(image, dim = 0)
 gen = UNet(input_dim, real_dim).to(device)
 gen_opt = torch.optim.Adam(gen.parameters(), lr=lr)
 disc = Discriminator(input_dim + real_dim).to(device)
 disc_opt = torch.optim.Adam(disc.parameters(), lr=lr)
+
+def generateExampleOutputs():
+    for i in range(1, 50):
+        inputFile = 'ExampleInput/' + str(i) + ".jpg"
+        outputFile = 'ExampleOutput/' + str(i) + ".jpg"
+        img = Image.open(inputFile)
+        convert_tensor = transforms.ToTensor()
+        image = convert_tensor(img)
+        image = torch.unsqueeze(image, dim = 0)
+        image_width = image.shape[3]
+        condition = image[:, :, :, :image_width // 2]
+        real = image[:, :, :, image_width // 2:]
+        condition = nn.functional.interpolate(condition, size=target_shape)
+        real = nn.functional.interpolate(real, size=target_shape)
+        condition = condition.to(device)
+        real = real.to(device)
+        with torch.no_grad():
+            fake = gen(condition)
+        a = show_tensor_images(fake, size=(real_dim, target_shape, target_shape))
+        b = show_tensor_images(real, size=(real_dim, target_shape, target_shape))
+        c = show_tensor_images(condition, size=(real_dim, target_shape, target_shape))
+        b = torch.cat([a, c, b], axis = 1)
+        plt.imshow(b)
+        plt.savefig(outputFile)
 
 loaded_state = torch.load("myModel6.pth")
 gen.load_state_dict(loaded_state["gen"])
 gen_opt.load_state_dict(loaded_state["gen_opt"])
 disc.load_state_dict(loaded_state["disc"])
 disc_opt.load_state_dict(loaded_state["disc_opt"])
+generateExampleOutputs()
 
-image_width = image.shape[3]
-condition = image[:, :, :, :image_width // 2]
-real = image[:, :, :, image_width // 2:]
-condition = nn.functional.interpolate(condition, size=target_shape)
-real = nn.functional.interpolate(real, size=target_shape)
-condition = condition.to(device)
-real = real.to(device)
-with torch.no_grad():
-    fake = gen(condition)
-a = show_tensor_images(fake, size=(real_dim, target_shape, target_shape))
-b = show_tensor_images(real, size=(real_dim, target_shape, target_shape))
-c = show_tensor_images(condition, size=(real_dim, target_shape, target_shape))
-b = torch.cat([a, c, b], axis = 1)
-plt.imshow(b)
-plt.savefig('exampleOut.jpg')
-plt.show()
+
